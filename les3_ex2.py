e60abcf9b8ee6871e5c80e064be5b1566f384dce
l-natali <97965789+l-natali@users.noreply.github.com>
2022-01-19 23:45:17 +0200

    Add files via upload

---
+++ les3_ex2.py
@@ -0,0 +1,7 @@
+x = int(input('Print number '))
+if x < 20:
+    print('Value less than 20')
+elif x == 20:
+    print('Value is 20')
+else:
+    print('Value bigger than 20')
---
+++ les3_ex3.py
@@ -0,0 +1,7 @@
+x = int(input('Print number '))
+while x != 0:
+    if x == 0:
+        print('Number is 0')
+        break
+    x = int(input('Number is not 0 '))
+print('Number is 0')
