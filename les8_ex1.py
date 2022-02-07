86eec45849ae30a7400f9b9816e72b095b40e1d9
l-natali <lukienko.natasha@gmail.com>
2022-02-07 22:52:20 +0200

    lesson 5

---
+++ les8_ex1.py
@@ -0,0 +1,3 @@
+days = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Sunday', 7: 'Saturday'}
+day = int(input('Input week number - '))
+print(days.get(day))
---
+++ les8_ex2.py
@@ -0,0 +1,3 @@
+cat = {'breed': input('Breed is '), 'color': input('Color is '), 'name': input('Name is ')}
+for item in cat:
+    print(item, cat[item], sep=' - ')
